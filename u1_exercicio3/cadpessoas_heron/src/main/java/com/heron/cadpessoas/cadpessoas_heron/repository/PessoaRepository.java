46efdc0bf5db2037c52a633e66a611b217fddbcc
Programação WEB - UFERSA Angicos <pwebufersa@gmail.com>
2022-05-09 16:39:24 -0300

    Update PessoaRepository.java
    Corrigindo Unidade1. Estava faltando a anotação de repositório.

--- u1_exercicio3/cadpessoas_heron/src/main/java/com/heron/cadpessoas/cadpessoas_heron/repository/PessoaRepository.java
+++ u1_exercicio3/cadpessoas_heron/src/main/java/com/heron/cadpessoas/cadpessoas_heron/repository/PessoaRepository.java
@@ -1,8 +1,11 @@
 package com.heron.cadpessoas.cadpessoas_heron.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.heron.cadpessoas.cadpessoas_heron.model.Pessoa;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
     
 }
