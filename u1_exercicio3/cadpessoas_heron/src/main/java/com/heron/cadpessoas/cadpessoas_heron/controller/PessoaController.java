54d4b1ef268ca8fdb48eb86ed5962f8444751877
HeronCSS <88357706+HeronCSS@users.noreply.github.com>
2024-01-09 15:23:37 -0300

    update PP

--- u1_exercicio3/cadpessoas_heron/src/main/java/com/heron/cadpessoas/cadpessoas_heron/controller/PessoaController.java
+++ u1_exercicio3/cadpessoas_heron/src/main/java/com/heron/cadpessoas/cadpessoas_heron/controller/PessoaController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -118,6 +119,12 @@ public class PessoaController {
         ModelAndView mav = new ModelAndView("contato");
         mav.addObject("urlBackground", urlBackground);
         return mav;
+    }
+
+    @GetMapping("/mudarUrl")
+    public String url(@RequestParam("urlBackground")String url){
+        urlBackground = url;
+        return "redirect:/";
     }
 
 }
